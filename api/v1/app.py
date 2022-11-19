6748a43b55d6cb3e341544ef51a507b30c152d13
oolufolabii <otitoola.olufolabi@gmail.com>
2022-11-19 20:08:48 +0100

    API Update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
 '''Flask web application API.
 '''
-
 import os
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
+
 app = Flask(__name__)
 
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
-app.teardown_appcontext
+@app.teardown_appcontext
 def teardown_flask(exception):
     '''The Flask app end event listener.'''
     storage.close()
 
 
 @app.errorhandler(404)
-def error404():
-    """Function to handle the 404 Error"""
-    return jsonify(error="Not Found"), 404
+def error_404(error):
+    '''Function to handle the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
 
 
-if __name__ == "__main__":
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+if __name__ == '__main__':
     app.run(
         host=app_host,
         port=app_port,
