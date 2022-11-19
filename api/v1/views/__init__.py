db98f7ef86d6ec6eaccc2d86c203d9079a464190
oolufolabii <otitoola.olufolabi@gmail.com>
2022-11-19 09:51:58 +0100

    API

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Contains the blueprint for the API.'''
+from flask import Blueprint
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+'''The blueprint for the AirBnB clone API.'''
